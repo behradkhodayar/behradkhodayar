baa5c1cc4c9e1a265cf3b2b7903fcec784c7cdd7
behradkhodayar <behrad.khodayar@gmail.com>
2018-10-27 11:44:26 +0000

    Add Oracle blockchain SaaS post

--- app/blockchain/content.ts
+++ app/blockchain/content.ts
@@ -7,6 +7,14 @@
 import { type Post, byDateDesc } from "../lib/content";
 
 export const posts: Post[] = [
+  {
+    slug: "oracle-blockchain-cloud-service",
+    title: "Oracle Releases Its Blockchain SaaS Suite",
+    date: "2018-10-27",
+    excerpt:
+      "Oracle ships its Blockchain Applications Cloud — enterprise blockchain SaaS for supply chains, built on Hyperledger Fabric. Translated from BlockFarsi.",
+    tags: ["Enterprise", "News", "BlockFarsi"],
+  },
 ];
 
 /** Newest-first posts for listing. */
